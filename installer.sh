bdeade994a6b9e8d06385beab67f2220f98a8f15
Dario Seidl <darioseidl@fastmail.fm>
2018-10-27 12:00:41 +0200

    Update install script: install from this repo; install to ~/.local/bin

--- installer.sh
+++ installer.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
 ## Clone the repo
-git clone https://github.com/kamranahmedse/git-standup.git --depth=1 || { echo >&2 "Clone failed with $?"; exit 1; }
+cd /tmp
+git clone https://github.com/darioseidl/git-standup.git --depth=1 || { echo >&2 "Clone failed with $?"; exit 1; }
 cd git-standup
-make install || { echo >&2 "Clone failed with $?"; exit 1; }
-cd ..
-rm -rf git-standup
+make install PREFIX=~/.local || { echo >&2 "Make install failed with $?"; exit 1; }
+rm -rf /tmp/git-standup
